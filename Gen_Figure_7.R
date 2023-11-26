7792bd17d204c0c4e8e4519c3a14446ee15fa1af
yiqunchen <yiqunchen@berkeley.edu>
2023-11-25 22:54:28 -0800

    first pass all code

---
+++ Gen_Figure_7.R
@@ -0,0 +1,313 @@
+library(Seurat)
+library(DropletUtils)
+library(scater)
+library(ggfortify)
+library(patchwork)
+library(CADET)
+library(umap)
+library(SeuratDisk)
+library(zellkonverter)
+library(scuttle)
+### qq plot
+library(tidyverse)
+library(latex2exp)
+library(ggrepel)
+
+set.seed(2022)
+TS_bone_marrow <- zellkonverter::readH5AD("~/Downloads/TS_Bone_Marrow.h5ad",
+  verbose = T,
+  layers = F, varm = F, obsm = F, varp = F, obsp = F, uns = F
+)
+# all manually annotated
+table(TS_bone_marrow$manually_annotated)
+names(assays(TS_bone_marrow)) <- "counts"
+
+process_data <- function(sce) {
+  # Define mitochondrial, ribosomal protein genes
+  rowData(sce)$Mito <- grepl("^MT", rowData(sce)$Symbol)
+  rowData(sce)$ProteinRibo <- grepl("^RP", rowData(sce)$Symbol)
+
+  # Calculate quality statistics
+  sce <- addPerCellQC(sce, subsets = list(mt = rowData(sce)$Mito, rbp = rowData(sce)$ProteinRibo))
+  sce <- addPerFeatureQC(sce)
+
+  # Delete bad cells (low library size, low gene count, high mitochondrial read proportion)
+  libsize.drop <- isOutlier(sce$sum, nmads = 3, type = "lower", log = TRUE)
+  feature.drop <- isOutlier(sce$detected, nmads = 3, type = "both", log = TRUE)
+  mito.drop <- isOutlier(sce$subsets_mt_percent, nmads = 3, type = "higher", log = TRUE)
+  sce <- sce[, !(libsize.drop | feature.drop | mito.drop)]
+
+  # Normalize library sizes, then log2 transformation with pseudo-count of 1
+  sce <- logNormCounts(sce)
+
+  # Subset to top 500 average count genes
+  X <- t(logcounts(sce))
+  X <- as.matrix(X)
+  X <- X[, order(rowData(sce)$mean, decreasing = T)[1:500]]
+  return(list(data = X, labels = colData(sce)$CellType))
+}
+
+X_bone_marrow <- t(logcounts(logNormCounts(TS_bone_marrow)))
+organ_tissue <- TS_bone_marrow$organ_tissue
+# relevant features
+# cell types
+X1 <- X_bone_marrow[which(
+  (TS_bone_marrow$donor == "TSP14") &
+    (TS_bone_marrow$cell_ontology_class %in%
+      c("memory b cell", "monocyte", "nk cell", "macrophage"))
+), ]
+# throw an error for zero var genes
+var_X1 <- apply(X1, 2, var)
+mean_X1 <- apply(X1, 2, mean)
+X1 <- X1[, order(var_X1, decreasing = T)[1:500]]
+# prop of zeros
+zero_prop_X1 <- apply(X1, 2, mean)
+set.seed(1234)
+# pick best seed?
+# test_1 <- kmeans(X1,centers = 3)
+seed_list <- sample(2021, size = 30, replace = FALSE)
+k_means_list <- lapply(seed_list, function(x) {
+  kmeans_estimation(X1,
+    k = 4, 1, iter.max = 30, seed = x
+  )
+})
+within_ss_list <- lapply(k_means_list, function(x) x$objective[[x$iter]])
+best_seed <- seed_list[which.min(unlist(within_ss_list))]
+current_kmeans_k_3_neg <- k_means_list[[which.min(unlist(within_ss_list))]]
+final_cluster_k_3_neg <- current_kmeans_k_3_neg$cluster[[current_kmeans_k_3_neg$iter]]
+table(final_cluster_k_3_neg)
+
+final_centroids <- current_kmeans_k_3_neg$centers[[current_kmeans_k_3_neg$iter]]
+X1_demean <- X1 - final_centroids[final_cluster_k_3_neg, , drop = FALSE]
+cov_sample <- coop::covar(X1_demean)
+
+# this is just pcr
+set.seed(2021)
+X1_umap <- umap(X1)
+umap_data_x1 <- X1_umap$layout
+colnames(umap_data_x1) <- c("UMAP1", "UMAP2")
+
+
+# accumulate all the p-values
+selective_pvals_c1_c2 <- rep(NA, dim(X1)[2])
+selective_pvals_c1_c3 <- rep(NA, dim(X1)[2])
+selective_pvals_c2_c3 <- rep(NA, dim(X1)[2])
+selective_pvals_c2_c4 <- rep(NA, dim(X1)[2])
+selective_pvals_c1_c4 <- rep(NA, dim(X1)[2])
+selective_pvals_c3_c4 <- rep(NA, dim(X1)[2])
+
+naive_pvals_c1_c2 <- rep(NA, dim(X1)[2])
+naive_pvals_c1_c3 <- rep(NA, dim(X1)[2])
+naive_pvals_c2_c3 <- rep(NA, dim(X1)[2])
+naive_pvals_c2_c4 <- rep(NA, dim(X1)[2])
+naive_pvals_c1_c4 <- rep(NA, dim(X1)[2])
+naive_pvals_c3_c4 <- rep(NA, dim(X1)[2])
+
+
+final_cell_types <- TS_bone_marrow$cell_ontology_class[which(
+  (TS_bone_marrow$donor == "TSP14") &
+    (TS_bone_marrow$cell_ontology_class %in%
+      c("memory b cell", "monocyte", "nk cell", "macrophage"))
+)]
+
+table(final_cell_types, final_cluster_k_3_neg)
+
+for (i in c(1:dim(X1)[2])) {
+  cat("i", i, "\n")
+
+  cl_1_4_feat_i <- kmeans_inference_1f(X1,
+    k = 4, 1, 4,
+    feat = i, iso = FALSE, sig = NULL,
+    covMat = cov_sample, seed = best_seed,
+    iter.max = 30
+  )
+  selective_pvals_c1_c4[i] <- cl_1_4_feat_i$pval
+  naive_pvals_c1_c4[i] <- cl_1_4_feat_i$p_naive
+
+  cl_3_4_feat_i <- kmeans_inference_1f(X1,
+    k = 4, 3, 4,
+    feat = i, iso = FALSE, sig = NULL,
+    covMat = cov_sample, seed = best_seed,
+    iter.max = 30
+  )
+  selective_pvals_c3_c4[i] <- cl_3_4_feat_i$pval
+  naive_pvals_c3_c4[i] <- cl_3_4_feat_i$p_naive
+
+  cl_1_2_feat_i <- kmeans_inference_1f(X1,
+    k = 4, 1, 2,
+    feat = i, iso = FALSE, sig = NULL,
+    covMat = cov_sample, seed = best_seed,
+    iter.max = 30
+  )
+  selective_pvals_c1_c2[i] <- cl_1_2_feat_i$pval
+  naive_pvals_c1_c2[i] <- cl_1_2_feat_i$p_naive
+
+  cl_2_3_feat_i <- kmeans_inference_1f(X1,
+    k = 4, 2, 3,
+    feat = i, iso = FALSE, sig = NULL,
+    covMat = cov_sample, seed = best_seed,
+    iter.max = 30
+  )
+  selective_pvals_c2_c3[i] <- cl_2_3_feat_i$pval
+  naive_pvals_c2_c3[i] <- cl_2_3_feat_i$p_naive
+
+
+  cl_1_3_feat_i <- kmeans_inference_1f(X1,
+    k = 4, 1, 3,
+    feat = i, iso = FALSE, sig = NULL,
+    covMat = cov_sample, seed = best_seed,
+    iter.max = 30
+  )
+  selective_pvals_c1_c3[i] <- cl_1_3_feat_i$pval
+  naive_pvals_c1_c3[i] <- cl_1_3_feat_i$p_naive
+
+  cl_2_4_feat_i <- kmeans_inference_1f(X1,
+    k = 4, 2, 4,
+    feat = i, iso = FALSE, sig = NULL,
+    covMat = cov_sample, seed = best_seed,
+    iter.max = 30
+  )
+  selective_pvals_c2_c4[i] <- cl_2_4_feat_i$pval
+  naive_pvals_c2_c4[i] <- cl_2_4_feat_i$p_naive
+}
+
+plot_output_dir <- ""
+
+
+
+df_p0_summary <- data.frame(
+  p_select = c(
+    selective_pvals_c1_c2, selective_pvals_c2_c3,
+    selective_pvals_c1_c3,
+    selective_pvals_c2_c4,
+    selective_pvals_c3_c4, selective_pvals_c1_c4
+  ),
+  p_naive = c(
+    naive_pvals_c1_c2, naive_pvals_c2_c3,
+    naive_pvals_c1_c3, naive_pvals_c2_c4,
+    naive_pvals_c1_c4, naive_pvals_c3_c4
+  ),
+  cluster_1 = rep(c(1, 2, 1, 2, 3, 1), each = dim(X1)[2]),
+  cluster_2 = rep(c(2, 3, 3, 4, 4, 4), each = dim(X1)[2])
+) %>%
+  mutate(cluster_pair = paste0(cluster_1, "-", cluster_2))
+
+
+df_p0_plot <- df_p0_summary %>%
+  pivot_longer(-c(cluster_1, cluster_2, cluster_pair), names_to = "p_type", values_to = "p_values") %>%
+  mutate(p_type = as.factor(p_type)) %>%
+  group_by(p_type) %>%
+  mutate(theoretical = ecdf(p_values)(p_values))
+
+png("Figure_7_a.png",
+  width = 7.5, height = 7.5, res = 300, units = "in"
+)
+ggplot(data = df_p0_plot) +
+  geom_point(aes(x = theoretical, y = p_values, colour = p_type), size = 0.6) +
+  ylab("P-value Quantiles") +
+  xlab("Uniform(0,1) Quantiles") +
+  # ggtitle("P-values under the alternative")+
+  scale_x_continuous(limits = c(0, 1)) +
+  scale_y_continuous(limits = c(0, 1)) +
+  geom_abline(intercept = 0, slope = 1) +
+  labs(color = "Test") +
+  scale_color_manual(
+    values = c("#374E55FF", "#DF8F44FF"),
+    labels = unname(TeX(c(
+      "$\\hat{p}_{j,naive}$",
+      "$\\hat{p}_{j,k-means}$"
+    )))
+  ) +
+  theme_bw() +
+  theme(
+    plot.title = element_text(hjust = 0.5, size = 15),
+    legend.position = "bottom",
+    legend.title = element_text(size = 18),
+    axis.text = element_text(size = 15),
+    legend.text = element_text(size = 23, hjust = 0),
+    axis.title = element_text(size = 15)
+  ) +
+  guides(colour = guide_legend(override.aes = list(size = 5)))
+dev.off()
+
+
+df_p0_summary <- df_p0_summary %>% drop_na()
+# df_p0_summary$gene_names <- rep(names(X1), times = 6)
+group_BH <- df_p0_summary %>%
+  group_by(cluster_pair) %>%
+  group_modify(~ broom::tidy(p.adjust(.x$p_select, method = "BH"))) %>%
+  pull(x)
+
+group_BH_naive <- df_p0_summary %>%
+  group_by(cluster_pair) %>%
+  group_modify(~ broom::tidy(p.adjust(.x$p_naive, method = "BH"))) %>%
+  pull(x)
+# try to understand which genes are differentiually expressed
+bh_adjusted_select_pvalues <- p.adjust(df_p0_summary$p_select, method = "BH")
+bh_adjusted_naive_pvalues <- p.adjust(df_p0_summary$p_naive, method = "BH")
+## plot BH against FDR control
+df_p0_summary$bh_adjusted_select_pvalues_by_pair <- group_BH
+df_p0_summary$bh_adjusted_naive_pvalues_by_pair <- group_BH_naive
+fdr_cutoff <- seq(0, 1, by = 0.05)
+cluster_pairs <- unique(df_p0_summary$cluster_pair)
+# agg data by cluster pair
+plot_fdr_null_num_rej_by_pair <- data.frame()
+for (cluster in cluster_pairs) {
+  fdr_selective_rej <- sapply(fdr_cutoff, function(x) {
+    sum(df_p0_summary %>% filter(cluster_pair == cluster) %>% pull(bh_adjusted_select_pvalues_by_pair) <= x)
+  })
+  fdr_naive_rej <- sapply(fdr_cutoff, function(x) {
+    sum(df_p0_summary %>% filter(cluster_pair == cluster) %>% pull(bh_adjusted_naive_pvalues_by_pair) <= x)
+  })
+  plot_fdr_null_num_rej <- data.frame(
+    fdr_cutoff, fdr_selective_rej, fdr_naive_rej,
+    cluster
+  )
+  plot_fdr_null_num_rej_by_pair <- rbind(plot_fdr_null_num_rej_by_pair, plot_fdr_null_num_rej)
+}
+
+plot_fdr_null_num_rej_by_pair_df <- plot_fdr_null_num_rej_by_pair %>%
+  pivot_longer(c(fdr_selective_rej, fdr_naive_rej),
+    names_to = "method",
+    values_to = "num_of_rejections"
+  )
+
+plot_fdr_null_num_rej_df <- plot_fdr_null_num_rej %>%
+  mutate(difference = fdr_naive_rej - fdr_selective_rej) %>%
+  pivot_longer(-c(fdr_cutoff, cluster),
+    names_to = "method",
+    values_to = "num_of_rejections"
+  )
+
+
+png("Figure_7_b.png",
+  width = 12, height = 8, res = 300, units = "in"
+)
+ggplot(
+  plot_fdr_null_num_rej_by_pair_df %>%
+    filter(fdr_cutoff <= 0.25),
+  aes(x = fdr_cutoff, y = num_of_rejections, colour = method),
+) +
+  geom_line(size = 1.5) +
+  ylab("Number of rejected null hypotheses") +
+  xlab("Nominal FDR levels") +
+  # ggtitle("Bone marrow CD 4 alpha/beta, Donor 14")+
+  labs(color = "Test") +
+  scale_color_jama(labels = unname(TeX(c(
+    "BH-adjusted $\\hat{p}_{j,naive}$",
+    "BH-ajusted $\\hat{p}_{j,k-means}$"
+  )))) +
+  scale_linetype_manual(values = c("solid", "solid")) +
+  # scale_size_discrete(position="none")+
+  theme_bw() +
+  theme(
+    plot.title = element_text(hjust = 0.5, size = 15),
+    legend.position = "bottom",
+    legend.title = element_text(size = 15),
+    axis.text = element_text(size = 9),
+    legend.text = element_text(size = 18, hjust = 0),
+    axis.title = element_text(size = 12)
+  ) +
+  guides(colour = guide_legend(override.aes = list(size = 3))) +
+  facet_wrap(. ~ cluster, nrow = 2)
+dev.off()
--- README.md
+++ README.md
@@ -20,45 +20,27 @@ Rscript ./Gen_Figure_1.R
 ```
 Rscript ./Gen_Figure_2.R
 ```
-3. Generate data used on Figures 3-4 (simulated data sets to assess empirical Type I error and power).
+3. Generate data used on Figures 3 and 4 (simulated data sets to assess empirical Type I error and power). This step can be computationally intensive!
+``` 
+Rscript ./Gen_Data_Figure_3.R
+Rscript ./Gen_Data_Figure_4.R
 ```
-Rscript ./Figure_3_plot.R
+4. Generate Figure 3 (demonstrate selective Type I error control ). 
 ```
-4. Generate Figure 4 (demonstrate selective Type I error control). This step can be computationally intensive! The first R script runs the simulation study  described in Section 5.2 of our manuscript. This step might be computationally intensive!
+Rscript ./Plot_Figure_3.R
 ```
-Rscript ./sim_type_1_data.R
-Rscript ./sim_type_1_plot.R
+5. Generate Figures 4, 5, and Supplementary Figure 1 (conditional power, detection probability, and the regression-spline-based estimate of power, respectively). 
 ```
-5. Generate Figures 5 and 8 (detection probability, conditional power, and the regression-spline-based estimate of power). The first R script runs the simulation study  described in Section 5.3 of our manuscript. This step might be computationally intensive!
+Rscript ./Plot_Figure_4.R 
 ```
-Rscript ./sim_power_data.R 
-Rscript ./detect_p_power_plot.R
+6. Generate Figures 6 and 7 (analysis of the single-cell data). You need to install the relevant single-cell packages in `install_packages.R`, and download the [Bone Marrow single cell data set](https://figshare.com/ndownloader/files/34701967) from the Tabular Sapiens consortium.
 ```
-6. Generate Figure 6 (analysis of the Palmer penguins data). You need to install the `palmerpenguins` package.
-```
-Rscript ./Figure_6.R
-```
-7. Generate Figure 7 (analysis of the MNIST data). You need the `keras` package installed.
-```
-Rscript ./Figure_7.R
-```
-8. Generate Table 1 and Figure 10 (analysis of the single-cell data). You need to install the relevant single-cell packages in `install_packages.R`, in addition to download the single cell datasets from Zheng et al. (2017).
-In the code block below, we will make use of the data of the following five cell types: [monocytes](https://cf.10xgenomics.com/samples/cell-exp/1.1.0/cd14_monocytes/cd14_monocytes_filtered_gene_bc_matrices.tar.gz), [memory T cells](https://cf.10xgenomics.com/samples/cell-exp/1.1.0/memory_t/memory_t_filtered_gene_bc_matrices.tar.gz), [naive T cells](https://cf.10xgenomics.com/samples/cell-exp/1.1.0/naive_t/naive_t_filtered_gene_bc_matrices.tar.gz), [B cells](https://cf.10xgenomics.com/samples/cell-exp/1.1.0/b_cells/b_cells_filtered_gene_bc_matrices.tar.gz), and [natural killer cells](https://cf.10xgenomics.com/samples/cell-exp/1.1.0/cd56_nk/cd56_nk_filtered_gene_bc_matrices.tar.gz).
-
-After downloading the data, you will need to need to rename the five downloaded folders as `filtered_matrices_mex_mono`,  `filtered_matrices_mex_memory`, `filtered_matrices_mex_naive_cytotoxic`,`filtered_matrices_mex_bcell`, and `filtered_matrices_mex_nk`. The following analysis also assumes that all five folders are listed under the directory `raw`.
-```
-Rscript ./Table_1_and_Figure_10.R
-```
-9. Generate Figure 9. The first R script runs the simulation study  described in Appendix A.8 of our manuscript and might be computationally intensive. 
-```
-Rscript ./sim_power_data_less_sparse.R
-Rscript ./detect_p_power_plot_q_50.R
-Rscript ./detect_p_power_plot_less_sparse.R
+Rscript ./Gen_Figure_6.R
+Rscript ./Gen_Figure_7.R
 ```
 
 ### Reference: 
-- Chen YT and Witten DM. (2022+) Selective inference for k-means clustering. arXiv preprint. https://arxiv.org/abs/xxxx.xxxxx.
-- Zheng, G. X. Y., Terry, J. M., Belgrader, P., Ryvkin, P., Bent, Z. W., Wilson, R., Ziraldo, S. B., Wheeler, T. D., McDermott, G. P., Zhu, J., Gregory, M. T., Shuga, J., Montesclaros, L., Underwood, J. G., Masquelier, D. A., Nishimura, S. Y., Schnall-Levin, M., Wyatt, P. W., Hindson, C. M., Bharadwaj, R., Wong, A., Ness, K. D., Beppu, L. W., Deeg, H. J., McFarland, C., Loeb, K. R., Valente, W. J., Ericson, N. G., Stevens, E. A., Radich, J. P., Mikkelsen, T. S., Hindson, B. J., and Bielas, J. H. (2017), “Massively parallel digital transcriptional profiling of single cells,” Nature Communications, 8, 14049. https://doi.org/10.1038/ncomms14049.
+- Chen YT and Gao LL. (2023+) Testing for a difference in means of a single feature after clustering. arXiv preprint. 
 
 
 
